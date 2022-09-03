24e08070ce052b8eb8071b9f8645191a734ceeb9
Daniel Gilhuly <jdgilhuly@icloud.com>
2022-09-03 16:13:27 -0700

    Initial Commit

---
+++ BetTracker/BetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetTrackerApp.swift
+//  BetTracker
+//
+//  Created by John Gilhuly on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
